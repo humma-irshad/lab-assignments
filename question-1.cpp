42fd727edd794998485d8e044d11ffb5c5c0448b
humma-irshad <76107314+humma-irshad@users.noreply.github.com>
2022-01-18 18:42:07 +0530

    Create question-1.cpp

---
+++ question-1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+    int array[100];
+    
+    cout << "Enter the size of your collection: ";
+    cin >> size;
+    
+    cout << "Enter the values of your collection:\n";
+    for(int i = 0; i <= size - 1; i++)
+        cin >> array[i];
+    
+    // largest value
+    int max = array[0];
+    for(int i = 0; i <= size - 1; i++) {
+        if(max < array[i]) {
+            max = array[i];
+        }
+    }
+    
+    // smallest value
+    int min = array[0];
+    for(int i = 0; i <= size - 1; i++) {
+        if(min > array[i]) {
+            min = array[i];
+        }
+    }
+    
+    // average value
+    int avg, sum = 0;
+    for(int i = 0; i <= size - 1; i++) {
+        sum += array[i];
+    }
+    avg = sum / size;
+    
+    // displaying the values
+    cout << "Largest value: " << max << "\nSmallest value: " << min << "\nAverage value: " << avg;
+}
